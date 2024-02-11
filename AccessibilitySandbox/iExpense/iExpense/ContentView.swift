db375d607df71b010516872a0446be4771885022
Tamás Scheuring <102406235+scheuringtamas@users.noreply.github.com>
2024-02-11 16:13:28 +0100

    Add files via upload

---
+++ AccessibilitySandbox/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,95 @@
+import SwiftUI
+import Observation
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+                List {
+                    Section("Personal expenses") {
+                        ForEach(expenses.itemsPersonal) { item in
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                }
+
+                                Spacer()
+                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                    .modifier(Custom(amount: Int(item.amount)))
+                            }
+                            .accessibilityElement()
+                            .accessibilityLabel("\(item.name), \(item.amount) USD")
+                            .accessibilityHint(item.type)
+                        }
+                        .onDelete(perform: removePersonalItems)
+                    }
+                    
+                    Section("Business expenses"){
+                        ForEach(expenses.itemsBusiness) { item in
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                }
+                                
+                                Spacer()
+                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                    .modifier(Custom(amount: Int(item.amount)))
+                            }
+                            .accessibilityElement()
+                            .accessibilityLabel("\(item.name), \(item.amount) USD")
+                            .accessibilityHint(item.type)
+                        }
+                        .onDelete(perform: removeBusinessItems)
+                    }
+                }
+                .navigationTitle("iExpense")
+                .toolbar {
+                    Button("Add Expense", systemImage: "plus") {
+                        showingAddExpense = true
+                    }
+                }
+                .sheet(isPresented: $showingAddExpense) {
+                    AddView(expenses: expenses)
+                }
+        }
+    }
+    
+    func removePersonalItems(at offsets: IndexSet) {
+        expenses.itemsPersonal.remove(atOffsets: offsets)
+    }
+    
+    func removeBusinessItems(at offsets: IndexSet) {
+        expenses.itemsBusiness.remove(atOffsets: offsets)
+    }
+}
+
+struct Custom: ViewModifier {
+    let amount: Int
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(getColor())
+    }
+    
+    func getColor() -> Color {
+        if(amount < 10) {
+            return Color.blue
+        } else if(amount < 100) {
+            return Color.red
+        } else {
+            return Color.green
+        }
+    }
+}
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ AccessibilitySandbox/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
