e0f4027914ecbfc90c9a5e1bfa840a55037633b7
Tamás Scheuring <102406235+scheuringtamas@users.noreply.github.com>
2024-02-23 08:31:10 +0100

    Update Favorites.swift

--- SnowSeeker/SnowSeeker/Favorites.swift
+++ SnowSeeker/SnowSeeker/Favorites.swift
@@ -1,14 +1,11 @@
 import SwiftUI
 
 class Favorites: ObservableObject {
-    // the actual resorts the user has favorited
     private var resorts: Set<String>
 
-    // the key we're using to read/write in UserDefaults
     private let saveKey = "Favorites"
 
     init() {
-        // load our saved data
         if let data = UserDefaults.standard.data(forKey: saveKey) {
             if let decoded = try? JSONDecoder().decode(Set<String>.self, from: data) {
                 resorts = decoded
@@ -16,23 +13,19 @@ class Favorites: ObservableObject {
             }
         }
 
-        // still here? Use an empty array
         resorts = []
     }
 
-    // returns true if our set contains this resort
     func contains(_ resort: Resort) -> Bool {
         resorts.contains(resort.id)
     }
 
-    // adds the resort to our set, updates all views, and saves the change
     func add(_ resort: Resort) {
         objectWillChange.send()
         resorts.insert(resort.id)
         save()
     }
 
-    // removes the resort from our set, updates all views, and saves the change
     func remove(_ resort: Resort) {
         objectWillChange.send()
         resorts.remove(resort.id)
@@ -40,7 +33,6 @@ class Favorites: ObservableObject {
     }
 
     func save() {
-        // write out our data
         if let encoded = try? JSONEncoder().encode(resorts) {
             UserDefaults.standard.set(encoded, forKey: saveKey)
         }
